d1348153aa896815d6dbd6846fcd52a30375b734
Rain-edge <3960578768@qq.com>
2026-06-01 16:53:20 +0800

    测试CI|CD流水线

--- myapp/views.py
+++ myapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 def index(request):
     """这是我们的第一个视图函数：返回一行文本"""
-    return HttpResponse("Hello from DevToProd! 🚀 我的第一个 Django 应用成功运行了！")
+    return HttpResponse("Hello from DevToProd! 🚀 这是通过 CI/CD 自动部署的版本！")
